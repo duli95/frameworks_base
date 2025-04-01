f8d9c4b8fffcc31a7513cbc9b7ae3467dd1e2eaf
doanvtamhuynh <huynhdoan.2132@gmail.com>
2025-04-01 13:28:26 +0700

    SpoofSim: fix return statement

--- core/java/com/android/internal/util/custom/SpoofSim.java
+++ core/java/com/android/internal/util/custom/SpoofSim.java
@@ -36,6 +36,7 @@ public class SpoofSim {
                     return true;
                 }
             }
+            return false;
         } catch (Exception e) {
             return false;
         }
